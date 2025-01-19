37850b2b69d08260735a76aa3e8bf6e9ecd229f9
SolidCode109 <krzysztof.siczek109@gmail.com>
2025-01-19 21:07:33 +0100

    pets by id changes

--- resources/views/pets/getPetsById.blade.php
+++ resources/views/pets/getPetsById.blade.php
@@ -1,12 +1,10 @@
 <div id="container--getPetsById">
     <h1>Pet by ID</h1>
     @if(isset($petsByID) && count($petsByID) > 0)
-        <ul>
             @foreach($petsByID as $pet)         
-                <li>{{ $pet['name'] ?? 'No name' }}</li>
+                <p><b>{{ $pet['name'] ?? 'No name' }}</b></p>
                 {{ var_dump($pet) }}
             @endforeach
-        </ul>
     @else
         <p>No pets available or an error occurred.</p>
     @endif
